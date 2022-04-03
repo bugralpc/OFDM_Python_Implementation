597b86b56060c4c8e3117a8c0906716b9a12d54a
bugralpc <bugra.cevikgibi@gmail.com>
2022-04-03 13:17:32 +0300

    2-4-8 PSK modulations are added, phase noise and AWGN are added

--- OFDM_library.py
+++ OFDM_library.py
@@ -1,6 +1,16 @@
 import numpy as np
 
 
+def AddAWGNNoise(complexSymbols, noisePower):
+    noise = (np.random.randn(len(complexSymbols))) + 1j*np.random.randn(len(complexSymbols))/np.sqrt(2)
+    RxSymbols = complexSymbols + noise*np.sqrt(noisePower)
+    return RxSymbols
+
+def AddPhaseNoise(complexSymbols, phaseNoisePower):
+    phaseNoise = np.random.randn(len(complexSymbols))*phaseNoisePower
+    RxSymbols = complexSymbols*np.exp(1j*phaseNoise)
+    return RxSymbols
+
 class OFDMParameters:
     dataSCNumber = 0
     bitsPerSymbol = 0
@@ -19,32 +29,55 @@ class OFDMParameters:
         self.rightGuardSCNumber = rightGuardSCNumber
 
         self.dataSCNumber = self.FFTSize - (self.leftGuardSCNumber + self.rightGuardSCNumber)
+        self.bitsPerSymbol = int(np.log2(PSKOrder))
 
-        if self.PSKOrder == 2:
-            self.bitsPerSymbol = 1
-        elif self.PSKOrder == 4:
-            self.bitsPerSymbol = 2
-        elif self.PSKOrder == 8:
-            self.bitsPerSymbol = 3
-
-        OFDMParameters.generateComplexSymbols(self.dataSCNumber, self.bitsPerSymbol)
+        OFDMParameters.generateComplexSymbols(self.dataSCNumber, self.bitsPerSymbol, self.PSKOrder)
         self.generateOFDMSymbol(self.complexSymbols, self.FFTSize, self.CPSize, self.leftGuardSCNumber,
                                 self.rightGuardSCNumber)
 
     @classmethod
-    def generateComplexSymbols(cls, dataSCNumber, bitsPerSymbol):
+    def generateComplexSymbols(cls, dataSCNumber, bitsPerSymbol, PSKOrder):
         cls.randomBits = np.random.binomial(n=1, p=0.5, size=(dataSCNumber * bitsPerSymbol))
         randomBitsGroup = cls.randomBits.reshape(int(len(cls.randomBits) / bitsPerSymbol), bitsPerSymbol)
-        for i in range(0, len(randomBitsGroup)):
-            if np.array_equal(randomBitsGroup[i], [0, 0]):
-                cls.complexSymbols.append(+1 - 1j)
-            elif np.array_equal(randomBitsGroup[i], [0, 1]):
-                cls.complexSymbols.append(+1 + 1j)
-            elif np.array_equal(randomBitsGroup[i], [1, 0]):
-                cls.complexSymbols.append(-1 + 1j)
-            else:
-                cls.complexSymbols.append(-1 - 1j)
+        m = np.arange(0, PSKOrder)
+        I = 1 / np.sqrt(2) * np.cos(m / PSKOrder * 2 * np.pi)
+        Q = 1 / np.sqrt(2) * np.sin(m / PSKOrder * 2 * np.pi)
+        constellationMap = I + 1j * Q
 
+        if PSKOrder == 2:
+            for i in range(0, len(randomBitsGroup)):
+                if np.array_equal(randomBitsGroup[i], [0]):
+                    cls.complexSymbols.append(constellationMap[0])
+                else:
+                    cls.complexSymbols.append(constellationMap[1])
+        elif PSKOrder == 4:
+            for i in range(0, len(randomBitsGroup)):
+                if np.array_equal(randomBitsGroup[i], [0, 0]):
+                    cls.complexSymbols.append(constellationMap[0])
+                elif np.array_equal(randomBitsGroup[i], [0, 1]):
+                    cls.complexSymbols.append(constellationMap[1])
+                elif np.array_equal(randomBitsGroup[i], [1, 0]):
+                    cls.complexSymbols.append(constellationMap[2])
+                else:
+                    cls.complexSymbols.append(constellationMap[3])
+        elif PSKOrder == 8:
+            for i in range(0, len(randomBitsGroup)):
+                if np.array_equal(randomBitsGroup[i], [0, 0, 0]):
+                    cls.complexSymbols.append(constellationMap[0])
+                elif np.array_equal(randomBitsGroup[i], [0, 0, 1]):
+                    cls.complexSymbols.append(constellationMap[1])
+                elif np.array_equal(randomBitsGroup[i], [0, 1, 0]):
+                    cls.complexSymbols.append(constellationMap[2])
+                elif np.array_equal(randomBitsGroup[i], [0, 1, 1]):
+                    cls.complexSymbols.append(constellationMap[3])
+                elif np.array_equal(randomBitsGroup[i], [1, 0, 0]):
+                    cls.complexSymbols.append(constellationMap[4])
+                elif np.array_equal(randomBitsGroup[i], [1, 0, 1]):
+                    cls.complexSymbols.append(constellationMap[5])
+                elif np.array_equal(randomBitsGroup[i], [1, 1, 0]):
+                    cls.complexSymbols.append(constellationMap[6])
+                else:
+                    cls.complexSymbols.append(constellationMap[7])
     @classmethod
     def generateOFDMSymbol(cls, complexSymbols, FFTSize, CPSize, leftGuardSCNumber, rightGuardSCNumber):
         allCarriers = np.arange(0, FFTSize)
--- main.py
+++ main.py
@@ -6,12 +6,22 @@ OFDM1 = OFDM_library.OFDMParameters(64, 16, 15000, 4, 4, 4)
 
 print(OFDM1.OFDMFFTVector)
 
-plt.plot(abs(OFDM1.OFDMFFTVector))
+
+plt.stem(abs(OFDM1.OFDMFFTVector))
 plt.show()
 
 plt.plot(abs(OFDM1.CPOFDMVector))
 plt.show()
 
+plt.scatter(np.real(OFDM1.OFDMFFTVector), np.imag(OFDM1.OFDMFFTVector))
+plt.xlim([-1.5, 1.5])
+plt.ylim([-1.5, 1.5])
+plt.show()
 
+rxSymbols = OFDM_library.AddAWGNNoise(OFDM1.OFDMFFTVector, 0.005)
 
+plt.scatter(np.real(rxSymbols), np.imag(rxSymbols))
+plt.xlim([-1.5, 1.5])
+plt.ylim([-1.5, 1.5])
+plt.show()
 
